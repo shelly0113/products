0aa3d0c2297f05d4adbd69c21ac5307b5f0c673f
Xinyi Jiang <csy50114@gmail.com>
2023-06-09 00:40:04 +0800

    add function

--- products.py
+++ products.py
@@ -1,35 +1,55 @@
 import os
-products = []
 # 檢查檔案是否存在
-if os.path.isfile('products.csv'):
-    print('yes')
 
-    # 讀取檔案
-    with open('products.csv', 'r', encoding='utf-8') as f:
-        for line in f:
-            if '商品,價格' in line:
-                continue
-            name, price = line.strip().split(',')
-            products.append([name, price])
-    print(products)
-else:
-    print('no')
+
+def read_file(filename):
+    products = []
+    if os.path.isfile(filename):
+        print('yes')
+
+        # 讀取檔案
+        with open(filename, 'r', encoding='utf-8') as f:
+            for line in f:
+                if '商品,價格' in line:
+                    continue
+                name, price = line.strip().split(',')
+                products.append([name, price])
+        print(products)
+    else:
+        print('no')
+    return products
 
 # 讓使用者輸入
-while True:
-    name = input('請輸入商品名稱: ')
-    if name == 'q':
-        break
-    price = input('請輸入價格: ')
-    products.append([name, price])
-print(products)
+
+
+def user_input(products):
+    while True:
+        name = input('請輸入商品名稱: ')
+        if name == 'q':
+            break
+        price = input('請輸入價格: ')
+        products.append([name, price])
+    print(products)
+    return products
 
 # 印出所有購買紀錄
-for p in products:
-    print(p[0], '的價格是', p[1])
+
+
+def print_products(products):
+    for p in products:
+        print(p[0], '的價格是', p[1])
 
 # 寫入檔案
-with open('products.csv', 'w', encoding='utf-8') as f:
-    f.write('商品名稱, 價格\n')
-    for p in products:
-        f.write(p[0] + ',' + p[1] + '\n')
+
+
+def write_file(filename, products):
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write('商品名稱, 價格\n')
+        for p in products:
+            f.write(p[0] + ',' + p[1] + '\n')
+
+
+products = read_file('products.csv')
+products = user_input(products)
+print_products(products)
+write_file('products.csv', products)
